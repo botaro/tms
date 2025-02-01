139afc9770ef66711f0a164e0dbbe39af1506bf4
botaro <binbotaro3@gmail.com>
2025-02-01 17:48:40 +0800

    2025-02-01-17-48-40

--- cmd/rem.sh
+++ cmd/rem.sh
@@ -2,10 +2,11 @@ p=$(pwd)
 d=${p: -3}
 echo $d
 
-url=git@github.com:nakanoaniki/$d.git
+url=git@github.com:botaro/$d.git
 
+echo $url
 
 git remote set-url origin $url 
+git config remote.origin.url
 
 # git remote add origin git@github.com:botaro/$1.git
-git config remote.origin.url
